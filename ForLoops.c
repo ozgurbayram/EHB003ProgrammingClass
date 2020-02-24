3bb4065ea99cb48349be1defa296a5de40827ec4
ozgurbayram <ozgurbayram@protonmail.com>
2020-02-24 23:25:47 +0300

    foor loops added

---
+++ ForLoops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int gs;
+    int eb = 0;
+    for(int i=1;i<6;i++){
+        printf("Lütfen %d. Sayıyı Giriniz:",i);
+        scanf("%d",&gs);
+        if(gs>eb){
+            eb = gs;
+        }
+    }
+    printf("eb : %d \n",eb);
+}
