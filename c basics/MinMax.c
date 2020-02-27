e34d78def38ade936220a6a5b158abae9e403ff5
ozgurbayram <ozgurbayram@protonmail.com>
2020-02-27 22:48:08 +0300

    min max

---
+++ c basics/MinMax.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int input;
+    
+    printf("Lütfen Bir Sayı Giriniz:");
+    scanf("%d",&input);
+    
+    int max=input;
+    int min=input;
+
+    int x;
+
+    for(x=1;x<=5;x++){
+        printf("Lütfen Bir Sayı Giriniz:");
+        
+        scanf("%d",&input);
+        if(input>max){
+            max = input;
+        }
+        else if(input<min){
+            min = input;
+        }
+    }
+
+    printf("En Büyük Sayı: %d \n",max);
+    printf("En Küçük Sayı: %d \n",min);
+
+}
