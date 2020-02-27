a123ae733a4674f55a894555d34d06c27906b5b0
ozgurbayram <ozgurbayram@protonmail.com>
2020-02-27 22:26:15 +0300

    sum numbers 2

---
+++ c basics/SumNumbers2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int sum;
+    int x;
+    sum=0;
+
+    for(x=1;x<=5;x++){
+        sum =sum + x;
+        printf("%d \n",sum);
+    }
+    printf("Sum of numbers between 0 and 50:%d \n",sum);
+}
