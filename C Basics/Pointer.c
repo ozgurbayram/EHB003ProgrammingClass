f5119363c7790802eedaf17e7c2cad5c4b771f55
ozgurbayram <ozgurbayram@protonmail.com>
2020-03-09 23:09:10 +0300

    pointer

---
+++ C Basics/Pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i = 1;
+    int *j = &i;
+    
+    // i Değşkenin Bellekteki Adresini Bulur
+    printf("%u \n",j);
+
+    // i  Deiğşkeninin Değerini Yazdırır 
+    printf("%d \n",*j);
+    return 0;    
+
+}
